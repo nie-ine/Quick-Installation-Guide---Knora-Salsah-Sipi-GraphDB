c7d9a541c66620d66fb7e0966d9359efa46bddd7
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-06-28 09:33:30 +0200

    added importLatestOntology script

---
+++ importLatestOntology.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+git clone --depth=1 https://github.com/dhlab-basel/Knora.git  || true
+git clone --depth=1 https://github.com/nie-ine/Ontologies.git  || true
+
+# Copy admin data
+cp -fr ./Ontologies/NIE_Knora_ontology-dependency/admin-data.ttl ./Knora/webapi/_test_data/all_data/
+
+# Copy permissions data
+cp -fr ./Ontologies/NIE_Knora_ontology-dependency/permissions-data.ttl ./Knora/webapi/_test_data/all_data/
+
+# Copy Expect File
+cp -fr ./Ontologies/NIE_Knora_ontology-dependency/graphdb-knora-test-data.expect ./Knora/webapi/scripts/
+
+# Copy NIE-INE Ontologies
+cp  -R ./Ontologies/nie-ontologies/. ./Knora/webapi/_test_data/ontologies/
+
+cd Knora/webapi/scripts/ && ./graphdb-free-init-knora-test.sh && cd ../../..
