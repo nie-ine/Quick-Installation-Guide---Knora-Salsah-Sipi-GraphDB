ee8af9e70b5061d57bf7e526b563c11957d59cbb
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-08-03 15:01:04 +0200

    finished new release

--- Knora/Knora/webapi/src/main/scala/org/knora/webapi/app/package.scala
+++
@@ -1,8 +0,0 @@
-package org.knora.webapi
-
-package object app {
-
-    val APPLICATION_STATE_ACTOR_NAME = "applicationState"
-    val APPLICATION_STATE_ACTOR_PATH = "/user/" + APPLICATION_STATE_ACTOR_NAME
-
-}
