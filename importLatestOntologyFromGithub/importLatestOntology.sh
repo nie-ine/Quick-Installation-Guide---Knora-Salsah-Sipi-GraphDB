5c2b33c0347fdd2bc5c39c0ee884550c694a54dd
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2019-02-07 17:05:14 +0100

    new release

--- importLatestOntologyFromGithub/importLatestOntology.sh
+++ importLatestOntologyFromGithub/importLatestOntology.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
 git clone --depth=1 https://github.com/dhlab-basel/Knora.git  || true
-# git clone --depth=1 https://github.com/nie-ine/Ontologies.git  || true
+git clone --depth=1 https://github.com/nie-ine/Ontologies-shared.git  || true
 
 # Copy admin data
-cp -fr ./Ontologies_Sascha/NIE_Knora_ontology-dependency/admin-data.ttl ./Knora/webapi/_test_data/all_data/
+cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/admin-data.ttl ./Knora/webapi/_test_data/all_data/
 
 # Copy permissions data
-cp -fr ./Ontologies_Sascha/NIE_Knora_ontology-dependency/permissions-data.ttl ./Knora/webapi/_test_data/all_data/
+cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/permissions-data.ttl ./Knora/webapi/_test_data/all_data/
 
 # Copy Expect File
-cp -fr ./Ontologies_Sascha/NIE_Knora_ontology-dependency/graphdb-knora-test-data.expect ./Knora/webapi/scripts/
+cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/graphdb-knora-test-data.expect ./Knora/webapi/scripts/
+
+# Copy Template File
+cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/graphdb-free-knora-test-repository-config.ttl.tmpl ./Knora/webapi/scripts/
 
 # Copy NIE-INE Ontologies
-cp  -R ./Ontologies_Sascha/nie-ontologies/. ./Knora/webapi/_test_data/ontologies/
+cp  -R ./Ontologies-shared/nie-ontologies_shared/. ./Knora/webapi/_test_data/ontologies/
 
-# Copy adapted admin-data.ttl
-
-cp -fr ./admin-data.ttl ./Knora/webapi/_test_data/all_data/
-
-cd Knora/webapi/scripts/ && ./graphdb-free-docker-init-knora-test.sh && cd ../../..
+cd Knora/webapi/scripts/ && ./graphdb-free-docker-init-knora-test.sh && cd ../../..do ker
