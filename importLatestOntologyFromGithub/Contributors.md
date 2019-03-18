5fdea885f9e00de4bfd90e2693ea2f26112c0d0b
Sacha Kaufmann <sacha@kaufmann.lu>
2019-03-18 12:27:26 +0100

    Don’t rely on current Knora when update ontologies
    Don’t rely on current Knora version when update NIE-INE ontologies
    Therefore parts of Knora 5.0.0 are now used permanantely to download the latest versions of NIE-INE ontologies and upload them into the triplestore automatically.

---
+++ importLatestOntologyFromGithub/Contributors.md
@@ -0,0 +1,17 @@
+
+
+# Project Lead:
+
+    - Lukas Rosenthaler <https://github.com/lrosenth>
+
+# Contributors:
+
+    - Benjamin Geer <https://github.com/benjamingeer>
+    - Tobias Schweizer <https://github.com/tobiasschweizer>
+    - Ivan Subotic <https://github.com/subotic>
+    - Loïc Jaouen <https://github.com/loicjaouen>
+    - Sepideh Alassi <https://github.com/SepidehAlassi>
+    - Stefan Münnich <https://github.com/musicEnfanthen>
+    - Lukas Rosenthaler <https://github.com/lrosenth>
+    - André Kilchenmann <https://github.com/kilchenmann>
+    - <https://github.com/janCstoffregen>
--- importLatestOntologyFromGithub/importLatestOntology.sh
+++ importLatestOntologyFromGithub/importLatestOntology.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
-git clone --depth=1 https://github.com/dhlab-basel/Knora.git  || true
-git clone --depth=1 https://github.com/nie-ine/Ontologies-shared.git  || true
+# Clone shared ontologies from github
+cd ./ontologies/
+git clone --depth=1 https://github.com/nie-ine/Ontologies-shared.git || true
 
 # Copy admin data
-cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/admin-data.ttl ./Knora/webapi/_test_data/all_data/
+cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/admin-data.ttl ../data/
 
 # Copy permissions data
-cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/permissions-data.ttl ./Knora/webapi/_test_data/all_data/
+cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/permissions-data.ttl ../data/
 
 # Copy Expect File
-cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/graphdb-knora-test-data.expect ./Knora/webapi/scripts/
+cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/graphdb-knora-test-data.expect ../scripts/graphdb-knora-test-data.expect
 
 # Copy Template File
-cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/graphdb-free-knora-test-repository-config.ttl.tmpl ./Knora/webapi/scripts/
+cp -fr ./Ontologies-shared/NIE_Knora_ontology-dependency_shared/graphdb-free-knora-test-repository-config.ttl.tmpl ../scripts/
 
 # Copy NIE-INE Ontologies
-cp  -R ./Ontologies-shared/nie-ontologies_shared/. ./Knora/webapi/_test_data/ontologies/
+cp  -R ./Ontologies-shared/nie-ontologies_shared/. ./project-ontologies/
 
-cd Knora/webapi/scripts/ && ./graphdb-free-docker-init-knora-test.sh && cd ../../..
+cd ../scripts/ && ./graphdb-free-docker-init-knora-test.sh && cd ..
